6a00319d534367ceb5bae172c97713b3a86477bb
asimoneau <asimoneau@edmunds.com>
2017-04-06 09:54:33 -0700

    Output to Wavefront

--- dockerContainerFailure.py
+++ dockerContainerFailure.py
@@ -3,10 +3,24 @@ import boto3
 import argparse
 import logging
 from datetime import datetime, timedelta
+import json
+import time
+import requests
+import socket
 
 # Set up logger
 logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
+
+
+class clusterDefinition(object):
+    def __init__(self, name, region):
+        self.name = name
+        self.region = region
+        return
+
+    def toString(self):
+        return "Cluster name " + self.name + " region " + self.region
 
 
 def getServices(clusterName):
@@ -38,10 +52,11 @@ def getServices(clusterName):
     return services
 
 
-def unableEvents(events):
+def unableEvents(events, serviceName):
     if (verbose):
         print "events"
 
+    printServiceName = True
     numberFound = 0
     for event in events:
         message = event['message']
@@ -51,9 +66,12 @@ def unableEvents(events):
         createdAt = str(event['createdAt'])
         if ((monthDayToday in createdAt) or (monthDayYesterday in createdAt)):
             numberFound += 1
-            if (verbose):
-                print "\tmessage", message
-                print "\tcreatedAt", createdAt, "\n"
+            if (printServiceName):
+                print "serviceName", serviceName
+                printServiceName = False
+
+            print "\tmessage", message
+            print "\tcreatedAt", createdAt, "\n"
     return numberFound
 
 
@@ -63,21 +81,12 @@ def unableServices(services):
 
     numberFound = 0
     for service in services:
+        serviceName = service['serviceName']
         if (verbose):
             print service
-            print "serviceName", service['serviceName']
-#        print "runningCount", service['runningCount'], "desiredCount", service['desiredCount'], "pendingCount", service['pendingCount']
-#        print "placementConstraints", service['placementConstraints'], "placementStrategy", service['placementStrategy']
-#        print "status", service['status']
-#        print "taskDefinition", service['taskDefinition']
-#        print "loadBalancers", service['loadBalancers']
-#        #        print "roleArn", service['roleArn']
-#        print "createdAt", service['createdAt']
-#        #        print "clusterArn", service['clusterArn']
-#        #        print "serviceArn", service['serviceArn']
-#        print "deploymentConfiguration", service['deploymentConfiguration']
-#        #        print "deployments", service['deployments']
-        numberFound += unableEvents(service['events'])
+            print "serviceName", serviceName
+
+        numberFound += unableEvents(service['events'], serviceName)
     return numberFound
 
 
@@ -88,9 +97,6 @@ def findUnable(clusterName, service):
     services = []
     services.append(service)
     response = ecs.describe_services(cluster=clusterName, services=services)
-    #    for k, v in response.items():
-    #        print "key " + k
-    #        print(k, v)
     return unableServices(response['services'])
 
 
@@ -104,7 +110,6 @@ def printEvents(events):
 
 
 def printServices(services):
-#    print "services"
     for service in services:
 #        print service
         print "serviceName", service['serviceName']
@@ -148,11 +153,12 @@ def setDates():
     return
 
 
-def doService(clusterName):
+def doService(clusterName, region):
     if (verbose):
         print "doService", service
 
     setDates()
+
     if ((service != "ALL") and (service != "all")):
         if (verbose):
             printService(clusterName, service)
@@ -171,21 +177,90 @@ def doService(clusterName):
             printService(clusterName, serviceLocal)
         numberFoundService = findUnable(clusterName, serviceLocal)
         numberFound += numberFoundService
+        serviceName = serviceLocal.split("/")[1]
         if (numberFoundService > 0):
-            print "For service " + serviceLocal.split("/")[1] + " we found " + str(numberFoundService) + " failed tasks"
+            print "For service " + serviceName + " we found " + str(numberFoundService) + " failed tasks"
+            outputToWavefrontService(clusterName, region, serviceName, numberFoundService)
 #    services.append(service)
 #    response = ecs.describe_services(cluster=clusterName, services=services)
 #    for k, v in response.items():
 #        print "key " + k
 #        print(k, v)
 #    printServices(response['services'])
+    if (numberFound > 0):
+        print "Total found " + str(numberFound) + " failed tasks"
+        outputToWavefrontNoService(clusterName, region, numberFound)
     return numberFound
 
-def doit(clusterName):
-    numberFound = doService(clusterName)
+def outputToWavefrontService(cluster, region, serviceName, count):
+    createAndSendMetric(cluster, region, "." + serviceName, count)
+    return
+
+def outputToWavefrontNoService(cluster, region, count):
+    createAndSendMetric(cluster, region, "", count)
+    return
+
+def createAndSendMetric(cluster, region, serviceNameIfExists, count):
+    #com.edmunds.ops.aws.ecs.{REGION}.{CLUSTER_NAME}.{SERVICE_NAME}.tasks.error.{ERROR-KEY}.count
+    metric = "com.edmunds.ops.aws.ecs." + region + "." + cluster + serviceNameIfExists + ".tasks.error.unabletoplace.count"
+    if (liveEnvironment == 'prod-21'):
+        proxy_url = 'wavefront-proxy.prod-21.vip.aws2'
+    else:
+        proxy_url = 'wavefront-proxy.prod-11.vip.aws1'
+
+    s = socket.socket()
+    s.connect((proxy_url, 2878))
+    timestamp = int(time.time())
+    message = metric + " %d %d source=%s\n" % (count, timestamp, liveEnvironment)
+    print "Sending to Wavefront " + message
+    s.send(message)
+    s.close()
+    return
+
+def doit(clusterName, region):
+    numberFound = doService(clusterName, region)
     if (numberFound > 0):
         print "Overall we found " + str(numberFound) + " failed tasks"
     return
+
+
+def createClusters():
+    clusters = []
+    clusters.append(clusterDefinition("qa-11", "us-east-1"))
+    clusters.append(clusterDefinition("prod-11", "us-east-1"))
+    clusters.append(clusterDefinition("qa-21", "us-west-2"))
+    clusters.append(clusterDefinition("prod-21", "us-west-2"))
+    return clusters
+
+def getLiveEnvironment():
+    request = requests.get("http://emon-api.prod-admin11.vip.aws1/api/environments")
+
+    results = json.loads(request.text)
+
+    for env in results['data']:
+        if (env['is_live'] == True):
+            return env['name']
+    return "NothingLive"
+
+def unableHandler(event, context):
+    message = event[0]
+    print "Event is", message
+    global verbose
+    verbose = False
+    global service
+    service = "all"
+    global liveEnvironment
+    liveEnvironment = getLiveEnvironment()
+
+    clusters = createClusters()
+    for cluster in clusters:
+        print "doing cluster", cluster.toString()
+
+        global ecs
+        ecs = boto3.client('ecs', region_name=cluster.region)
+        doit(cluster.name, cluster.region)
+    return
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -206,12 +281,18 @@ if __name__ == "__main__":
         help='Guess'
     )
     parser.add_argument('-s', '--service',
-        nargs='?',
-        required=False,
-        default=None,
-        help='Will list the requested service'
+                        nargs='?',
+                        required=False,
+                        default=None,
+                        help='Will list the requested service'
     )
-    args = parser.parse_args()  
+    parser.add_argument('-l', '--lambdaAws',
+                        nargs='?',
+                        required=False,
+                        default=None,
+                        help='Whether to run Lambda'
+    )
+    args = parser.parse_args()
     regionName = args.region
 
     global ecs
@@ -221,5 +302,11 @@ if __name__ == "__main__":
     global service
     service = args.service
 
-    logging.info('Monitoring cluster %s on region %s...', args.cluster, regionName)
-    doit(args.cluster)
+    if (args.lambdaAws == 'True'):
+        event = ['dummy event']
+        context = None
+        unableHandler(event, context)
+    else:
+        logging.info('Monitoring cluster %s on region %s...', args.cluster, regionName)
+        doit(args.cluster, regionName)
+
